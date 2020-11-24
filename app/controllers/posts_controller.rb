adfa159b09b0273bb7df2b3888aeb07555c56492
akito1201 <akito.tennis@gmail.com>
2020-11-24 12:47:50 +0900

    formオブジェクト実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
   def new
     @post_plan = PostPlan.new
   end
+
+  def create
+    @post_plan = PostPlan.new(post_plan_params)
+    if @post_plan.valid?
+      @post_plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_plan_params
+    params.require(:post_plan).permit(:title, :outline, :prefecture_id, :city, :transportation_id, :member_id, :timing_id, :text, :place, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,12 @@
 class Post < ApplicationRecord
-  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :transportation
   belongs_to :member
-  belongs_to :time
-
-  with_options presence: true do
-    validates :title
-    validates :outline
-  end
-
-  with_options presence: true, numericality: { other_than: 1} do
-    validates :prefecture_id
-    validates :transportation_id
-    validates :member_id
-    validates :time_id
-  end
-
+  belongs_to :timing
+  
+  belongs_to :user
+  has_one :plan
 end
 
 
