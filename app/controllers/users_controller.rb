d899759d7839c8c3eddaa59a02be26008f06c813
akito1201 <akito.tennis@gmail.com>
2020-11-25 14:46:54 +0900

    ユーザー詳細ページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user_id: current_user.id)
+  end
+  
+  def show
+    @user = User.find_by(user_id: params[:user_id])
+    @posts = Post.where(user_id: @user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/:user_id/show', to: 'users#show'
+  get 'users/:user_id/index', to: 'users#index'
   root to: 'posts#index'
   devise_for :users
+
   resources :posts  do
     resources :plans
   end
