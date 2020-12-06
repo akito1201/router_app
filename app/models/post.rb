a5d742adae5351c45d0c4ac64126b00f574dcff9
akito1201 <akito.tennis@gmail.com>
2020-12-06 22:13:48 +0900

    validation設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,12 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :member
-  has_many :favorite_counts
+  has_many :favorite_counts, dependent: :destroy
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :plans, dependent: :destroy
+  
 
   with_options presence: true do
     validates :title
