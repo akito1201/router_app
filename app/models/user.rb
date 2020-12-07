b201f225ad7af7c574b6590cf8c47fc8842c77fa
akito1201 <akito.tennis@gmail.com>
2020-12-07 12:47:07 +0900

    お気に入り表示修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
   has_many :posts
   has_many :favorites
 
-  validates :nickname
+  validate :nickname
 end
