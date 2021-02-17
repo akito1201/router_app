268327c155bd42e264cc31ee67520be1a2b5df90
akito1201 <akito.tennis@gmail.com>
2021-02-17 09:28:34 +0900

    updated settings for rich text field

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,6 +56,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  ENV['RAILS_ENV'] = 'test'
+  require File.expand_path('../../config/environment', __FILE__)
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
   #   # This allows you to limit a spec run to individual examples or groups
